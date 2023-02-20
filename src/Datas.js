40745e60c1b3ef538205de1f3d2a17d9d739eb02
Michael Faleiro <michaelfaleiro@gmail.com>
2023-02-19 21:42:17 -0300

    innitial Commit

---
+++ src/Datas.js
@@ -0,0 +1,42 @@
+export const news = [
+  {
+    title: "Noticia 1",
+    text: "Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum",
+    image:
+      "https://acct.global/wp-content/uploads/2022/01/loren-ipsum-cover.jpg",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "Noticia 2",
+    text: "Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum",
+    image:
+      "https://acct.global/wp-content/uploads/2022/01/loren-ipsum-cover.jpg",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "Noticia 3",
+    text: "Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum",
+    image:
+      "https://acct.global/wp-content/uploads/2022/01/loren-ipsum-cover.jpg",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "Noticia 4",
+    text: "Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum",
+    image:
+      "https://acct.global/wp-content/uploads/2022/01/loren-ipsum-cover.jpg",
+    likes: 2,
+    comments: 4,
+  },
+  {
+    title: "Noticia 5",
+    text: "Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum Lorem Ipsum lorem ipsum",
+    image:
+      "https://acct.global/wp-content/uploads/2022/01/loren-ipsum-cover.jpg",
+    likes: 2,
+    comments: 4,
+  },
+];
